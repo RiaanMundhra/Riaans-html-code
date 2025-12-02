e6b6a40d574a95a1d8cfdee80316e51f88af3095
RiaanMundhra <165542789+RiaanMundhra@users.noreply.github.com>
2025-12-02 10:33:09 +0530

    Add files via upload

---
+++ ifstatements.py
@@ -0,0 +1,9 @@
+num = int(input("Enter any number you want : "))
+
+if num%2 == 0:
+    print("The number is even.") 
+else:
+    print("The number is odd.")
+
+
+print("Thank you for using the number checker.")
