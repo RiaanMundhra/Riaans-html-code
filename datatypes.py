fbb5ef512499e8f2fc9b55cc5e687909d049d5d4
RiaanMundhra <165542789+RiaanMundhra@users.noreply.github.com>
2025-12-23 16:56:03 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,16 @@
+a = 23
+print("the datatype of a is", type(a))
+b = "Riaan"
+print("the datatype of b is", type(b))
+c = True
+print("the datatype of c is", type(c))
+d = 45.67
+print("the datatype of d is", type(d))
+e = [1, 2, 3, 4, 5]
+print("the datatype of e is", type(e))
+f = (6, 7, 8, 9, 10)
+print("the datatype of f is", type(f))
+g = {11, 12, 13, 14, 15}
+print("the datatype of g is", type(g))
+h = {"name": "Riaan", "age": 23}
+print("the datatype of h is", type(h))
