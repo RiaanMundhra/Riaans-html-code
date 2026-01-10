92d9ef8ab17d012bdccfa152db65c2fcfa97cb82
RiaanMundhra <165542789+RiaanMundhra@users.noreply.github.com>
2026-01-10 11:53:30 +0530

    Add files via upload

---
+++ and&or.py
@@ -0,0 +1,13 @@
+a = 10
+b = 1
+c = 0
+if a & b & c == False:
+    print("Atleast 1 boolean value is false")
+else:
+    print("True")
+
+
+if a or b or c == True:
+    print("Atleast 1 boolean value is true")
+else:
+    print("False")
---
+++ not.py
@@ -0,0 +1,8 @@
+a = 10
+b = 20
+c = 10
+
+if a != b & b != c & a != c:
+    print("none of the variables are same")
+else:
+    print("Atleast 2 of the variables are same")
