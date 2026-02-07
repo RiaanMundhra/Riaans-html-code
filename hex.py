a6bedc501e1e3c6a49f6ef412497215035f472f8
RiaanMundhra <165542789+RiaanMundhra@users.noreply.github.com>
2026-02-07 12:09:37 +0530

    Add files via upload

---
+++ hex.py
@@ -0,0 +1,22 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("blue")
+screen.setup(400,300)
+
+
+Hex = turtle.Turtle()
+Hex.forward(300)
+Hex.right(60)
+Hex.forward(300)
+Hex.right(60)
+Hex.forward(300)
+Hex.right(60)
+Hex.forward(300)
+Hex.right(60)
+Hex.forward(300)
+Hex.right(60)
+Hex.forward(300)
+Hex.right(60)
+
+screen.exitonclick()
---
+++ superstar.py
@@ -0,0 +1,21 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("white")
+screen.setup(400,300)
+
+
+Hex = turtle.Turtle()
+
+Hex.left(108)
+Hex.forward(300)
+Hex.left(144)
+Hex.forward(300)
+Hex.left(144)
+Hex.forward(300)
+Hex.left(144)
+Hex.forward(300)
+Hex.left(144)
+Hex.forward(300)
+Hex.left(144)
+screen.exitonclick()
