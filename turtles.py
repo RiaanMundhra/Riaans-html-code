ade05b6798fb70b4a2a52936b2a008682aa3fb2b
RiaanMundhra <165542789+RiaanMundhra@users.noreply.github.com>
2026-02-09 14:49:19 +0530

    Add files via upload

---
+++ turtles.py
@@ -0,0 +1,21 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("red")
+screen.setup(400,300)
+
+# square
+
+canvas = turtle.Turtle()
+canvas.forward(350)
+canvas.left(90)
+canvas.forward(350)
+canvas.left(90)
+canvas.forward(350)
+canvas.left(90)
+canvas.forward(350)
+canvas.left(90)
+
+screen.exitonclick()
+
+
