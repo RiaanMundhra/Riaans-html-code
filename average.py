56a9e6ffc515e721e79da35b87d1ffc62eaaa219
RiaanMundhra <165542789+RiaanMundhra@users.noreply.github.com>
2026-03-04 10:22:07 +0530

    Add files via upload

--- average.py
+++ average.py
@@ -1,7 +1,5 @@
-tree1 = int(input("Enter the first tree height: "))
-tree2 = int(input("Enter the second tree height: "))
-tree3 = int(input("Enter the third tree height: "))
-tree4 = int(input("Enter the fourth tree height: "))
-tree5 = int(input("Enter the fifth tree height: "))
-average_height = (tree1 + tree2 + tree3 + tree4 + tree5) / 5
-print("The average tree height is:", average_height)
+lst = [1, 2, 3, 4, 5, 67, 8, 9, 10]
+count = 0
+for i in lst:
+    count = count+i
+print(count)
