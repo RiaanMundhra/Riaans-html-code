3207046675aae7fcb8ea4fa3df18a9a24acc4310
RiaanMundhra <165542789+RiaanMundhra@users.noreply.github.com>
2026-03-04 09:46:50 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,11 @@
+emptylist = []
+print(emptylist)
+
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(number)
+
+triples = [1, 5, 10, 5, 1] * 3
+print(triples)
+
+revorder = [1, 2, 3, 4, 5]
+print(revorder [::-1])
