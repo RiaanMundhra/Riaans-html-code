a9b42af08cd19b913218ebfa1d3c3384a032c2eb
RiaanMundhra <165542789+RiaanMundhra@users.noreply.github.com>
2026-03-10 10:49:29 +0530

    Add files via upload

---
+++ repeatition.py
@@ -0,0 +1,2 @@
+dictionary {"Name":2, "Age":2, "class":2, "address":1}
+K = 2
---
+++ unique.py
@@ -0,0 +1,6 @@
+sara = {"name":"sara", "age":"11", "standard":"5", "division":"C", "grade":"A"}
+linda = {"name":"linda", "age":"11", "standard":"5", "division":"C", "grade":"A"}
+sara1 = {"name":"sara", "age":"11", "standard":"5", "division":"C", "grade":"A"}
+
+if sara is linda | sara1:
+    print("sara is a duplicated name pls remove sara1")
