9d56aa2dd5bf0550af9d938a233a9273dd7ded54
RiaanMundhra <165542789+RiaanMundhra@users.noreply.github.com>
2026-05-02 12:24:34 +0530

    Add files via upload

---
+++ fruits.py
@@ -0,0 +1,10 @@
+class fruits:
+    def __init__(self, name, size, colour):
+        self.name = name
+        self.size = size
+        self.colour = colour
+
+aapple = fruits("apple", "small", "redish maroon")
+print(aapple.name)
+print(aapple.colour)
+print(aapple.size)
---
+++ morefruits.py
@@ -0,0 +1,18 @@
+class fruits:
+
+    taste = "Sweet"
+
+    def __init__(self, name, size, colour):
+        self.name = name
+        self.size = size
+        self.colour = colour
+
+aapple = fruits("apple", "small", "redish maroon")
+print(aapple.taste)
+print(aapple.name)
+print(aapple.colour)
+print(aapple.size)
+bananaj = fruits("banana", "giant", "yellow")
+print(bananaj.name)
+print(bananaj.colour)
+print(bananaj.size)
